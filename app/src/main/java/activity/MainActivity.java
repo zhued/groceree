056064425e1197c5559d32b1acff2561e6d6af06
joshfermin <joshfermin@gmail.com>
2015-09-11 22:40:51 -0600

    restructuring java folder and adding dependency for gradle file

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -1,10 +1,12 @@
-package com.example.groceree;
+package activity;
 
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.groceree.R;
 
 
 public class MainActivity extends ActionBarActivity {
