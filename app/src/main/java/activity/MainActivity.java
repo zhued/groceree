7b26ef0561f8306dc103690528ead8ed94130117
joshfermin <joshfermin@gmail.com>
2015-09-12 10:07:02 -0600

    sidebar now working

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -1,17 +1,25 @@
 package activity;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import  android.support.v4.app.Fragment;
+import android.view.View;
+
 
 import com.example.groceree.R;
 
+import adapter.FragmentDrawer;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends ActionBarActivity implements FragmentDrawer.FragmentDrawerListener {
 
     private Toolbar mToolbar;
+    private FragmentDrawer drawerFragment;
+//    private FragmentDrawer.FragmentDrawerListener drawerListener = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,12 @@ public class MainActivity extends ActionBarActivity {
 
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        // drawer fragment for sidebar:
+        drawerFragment = (FragmentDrawer)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+        drawerFragment.setDrawerListener(this);
     }
 
 
@@ -46,4 +60,71 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+
+    }
 }
+
+//import android.support.v4.widget.DrawerLayout;
+//        import android.support.v7.app.ActionBarActivity;
+//        import android.os.Bundle;
+//        import android.support.v7.widget.Toolbar;
+//        import android.view.Menu;
+//        import android.view.MenuItem;
+//        import android.view.View;
+//
+//import com.example.groceree.R;
+//
+//import adapter.FragmentDrawer;
+//
+//public class MainActivity extends ActionBarActivity implements FragmentDrawer.FragmentDrawerListener {
+//
+//    private Toolbar mToolbar;
+//    private FragmentDrawer drawerFragment;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+//
+//        setSupportActionBar(mToolbar);
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//
+//        drawerFragment = (FragmentDrawer)
+//                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+//        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+//        drawerFragment.setDrawerListener(this);
+//    }
+//
+//
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//
+//    @Override
+//    public void onDrawerItemSelected(View view, int position) {
+//
+//    }
+//}
