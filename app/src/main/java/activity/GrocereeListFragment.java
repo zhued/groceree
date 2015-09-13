88baeabd5b7e29ac1c9246d61e9dd6c262c99ed7
joshfermin <joshfermin@gmail.com>
2015-09-12 18:23:03 -0600

    added groceree list fragment

---
+++ app/src/main/java/activity/GrocereeListFragment.java
@@ -0,0 +1,44 @@
+package activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.groceree.R;
+
+/**
+ * Created by joshfermin on 9/12/15.
+ */
+public class GrocereeListFragment extends Fragment{
+    public GrocereeListFragment(){
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_groceree_list, container, false);
+
+
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -1,5 +1,7 @@
 package activity;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,10 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import  android.support.v4.app.Fragment;
 import android.view.View;
-
-
 import com.example.groceree.R;
-
 import adapter.FragmentDrawer;
 
 
@@ -19,7 +18,6 @@ public class MainActivity extends ActionBarActivity implements FragmentDrawer.Fr
 
     private Toolbar mToolbar;
     private FragmentDrawer drawerFragment;
-//    private FragmentDrawer.FragmentDrawerListener drawerListener = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +34,9 @@ public class MainActivity extends ActionBarActivity implements FragmentDrawer.Fr
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
         drawerFragment.setDrawerListener(this);
+
+        // display home on launch
+        displayView(0);
     }
 
 
@@ -63,68 +64,32 @@ public class MainActivity extends ActionBarActivity implements FragmentDrawer.Fr
 
     @Override
     public void onDrawerItemSelected(View view, int position) {
+        displayView(position);
+    }
 
+    private void displayView(int position){
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
+        switch(position){
+            case 0:
+                fragment = new HomeFragment();
+                title = getString(R.string.nav_item_home);
+                break;
+            case 1:
+                fragment = new GrocereeListFragment();
+                title = getString(R.string.nav_item_list);
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_body, fragment);
+            fragmentTransaction.commit();
+
+            // set the toolbar title
+            getSupportActionBar().setTitle(title);
+        }
     }
 }
-
-//import android.support.v4.widget.DrawerLayout;
-//        import android.support.v7.app.ActionBarActivity;
-//        import android.os.Bundle;
-//        import android.support.v7.widget.Toolbar;
-//        import android.view.Menu;
-//        import android.view.MenuItem;
-//        import android.view.View;
-//
-//import com.example.groceree.R;
-//
-//import adapter.FragmentDrawer;
-//
-//public class MainActivity extends ActionBarActivity implements FragmentDrawer.FragmentDrawerListener {
-//
-//    private Toolbar mToolbar;
-//    private FragmentDrawer drawerFragment;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//
-//        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-//
-//        setSupportActionBar(mToolbar);
-//        getSupportActionBar().setDisplayShowHomeEnabled(true);
-//
-//        drawerFragment = (FragmentDrawer)
-//                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
-//        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
-//        drawerFragment.setDrawerListener(this);
-//    }
-//
-//
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
-//
-//    @Override
-//    public void onDrawerItemSelected(View view, int position) {
-//
-//    }
-//}
